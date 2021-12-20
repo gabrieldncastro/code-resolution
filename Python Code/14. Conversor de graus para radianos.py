04e4367acc997ff6c3b4ecdd6bd7420fa46ab804
gabrieldncastro <79811029+gabrieldncastro@users.noreply.github.com>
2021-12-20 20:30:19 -0300

    submit

---
+++ Python Code/14. Conversor de graus para radianos.py
@@ -0,0 +1,6 @@
+"""Conversor de graus para radianos"""
+
+grau = float(input("Digite o valor do ângulo em graus: "))
+radiano = float(grau * 3.14 / 180)
+
+print(f'Um ângulo de {grau:.2f} graus equivale a: {radiano:.2f} radianos')
