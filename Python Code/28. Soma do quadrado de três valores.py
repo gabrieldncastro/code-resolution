7855ff8410fb60408a8754d3bed8c785c40ed5dd
gabrieldncastro <79811029+gabrieldncastro@users.noreply.github.com>
2021-12-24 20:55:35 -0300

    submit

--- Python Code/28. Soma do quadrado de três valores.py
+++ Python Code/28. Soma do quadrado de três valores.py
@@ -3,6 +3,6 @@ import math
 valor1 = int(input())
 valor2 = int(input())
 valor3 = int(input())
-resultado = ((valor1 * valor1) + (valor1 * valor1) + (valor1 * valor1))
+resultado = ((valor1 * valor1) + (valor2 * valor2) + (valor3 * valor3))
 
 print(f'A soma do quadrado dos valores: {valor1}, {valor2}, {valor3}, é {resultado}')
---
+++ Python Code/29.Média artmética entre 4 valores.py
@@ -0,0 +1,10 @@
+"""Média aritmética entre quatro notas"""
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+media = float((nota1 + nota2 + nota3 + nota4) / 4)
+
+print(f'{media:.1f}')
